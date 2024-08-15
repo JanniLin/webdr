5e5e55f4a09282235ead38201266533ec0721611
Yana Rusakova <arayennet@gmail.com>
2024-08-15 13:52:27 +0300

    2 browsers

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,14 +1,25 @@
+const rimraf = require('rimraf');
+const path = require('path');
+const { existsSync, mkdirSync } = require('fs');
+
+
 exports.config = {
     runner: 'local',
     specs: [
         './test/spec/**/*.js'
     ],
+
     exclude: [],
     maxInstances: 1,
-    capabilities: [{
-        browserName: 'chrome',
-        acceptInsecureCerts: true
-    }],
+    capabilities: [
+        {
+            browserName: 'firefox'
+        },
+        {
+            browserName: 'chrome'
+        }
+    ],
+
     logLevel: 'error',
     bail: 0,
     baseUrl: 'https://ej2.syncfusion.com/',
@@ -30,6 +41,38 @@ exports.config = {
             disableWebdriverScreenshotsReporting: false,
         }]
     ],
+    afterTest: async (test, context, result) => {
+        // take a screenshot anytime a test fails and throws an error
+        if (result.error) {
+            console.log(`Screenshot for the failed test ${test.title} is saved`);
+            const filename = test.title + '.png';
+            const dirPath = './artifacts/screenshots/';
+
+            if (!existsSync(dirPath)) {
+                mkdirSync(dirPath, {
+                    recursive: true,
+                });
+            }
+            await browser.saveScreenshot(dirPath + filename);
+        }
+    },
+    onComplete: function () {
+        const reportError = new Error('Could not generate Allure report');
+        const generation = allure(['generate', 'allure-results', '--clean']);
+
+        return new Promise((resolve, reject) => {
+            const generationTimeout = setTimeout(() => reject(reportError), 5000);
+            generation.on('exit', function (exitCode) {
+                clearTimeout(generationTimeout);
+
+                if (exitCode !== 0) {
+                    return reject(reportError);
+                }
+                console.log('Allure report successfully generated');
+                resolve();
+            });
+        });
+    },
     mochaOpts: {
         ui: 'bdd',
         timeout: 60000
